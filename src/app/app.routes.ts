055cdf6f819827fdb6b9517b01adf655fa165ee2
Nieves Reinado Charli Steven <113039225+NievesCharli@users.noreply.github.com>
2025-05-07 22:22:30 -0500

    ruta actualizada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { PokemonComponent } from './pages/pokemon/pokemon.component';
 import { DbzComponent } from './pages/dbz/dbz.component';
 import { ErrorComponent } from './pages/error/error.component';
-import { RickymortyComponent } from './pages/rickymorty/rickymorty.component';
+import { CoctelesComponent } from './pages/cocteles/cocteles.component';
 
 export const routes: Routes = [
 {path:'',redirectTo:'home',pathMatch:'full'},
 {path:'home',component:HomeComponent},
 {path: 'pokemon', component: PokemonComponent},
-{path:'rickymorty', component:RickymortyComponent},
+{path:'cocteles', component:CoctelesComponent},
 {path: 'dbz', component: DbzComponent},
 {path:'miapi',loadChildren:()=>import('./pages/miapi/miapi-routing.module').then(m=>m.MiapiRoutingModule)},
 {path:'**', component: ErrorComponent}
